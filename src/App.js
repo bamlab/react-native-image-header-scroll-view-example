b907324b6c857ff3d287891d8d714060181fd3fd
Nicolas Djambazian <nicolas@djambazian.fr>
2019-01-17 16:53:14 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+// @flow
+import { createStackNavigator } from "react-navigation";
+
+import {
+  Menu,
+  TvShow,
+  BasicUsage,
+  ColorsPage,
+  Avignon,
+  PullToRefresh
+} from "./Pages";
+
+export const App = createStackNavigator(
+  {
+    menu: {
+      screen: Menu
+    },
+    tvShow: {
+      screen: TvShow
+    },
+    basicUsage: {
+      screen: BasicUsage
+    },
+    colors: {
+      screen: ColorsPage
+    },
+    avignon: {
+      screen: Avignon
+    },
+    pullrefresh: {
+      screen: PullToRefresh
+    }
+  },
+  {
+    navigationOptions: {
+      headerTintColor: "white",
+      headerTransparent: true
+    }
+  }
+);
+
+export default App;
