d3bf44a214291a646e6e96bae1a47b365e9740b4
Nicolas Djambazian <nicolas@djambazian.fr>
2019-10-08 17:41:11 +0200

    Start implementing a Twitter example

--- src/Twitter.tsx
+++ src/Twitter.tsx
@@ -1,75 +1,61 @@
-// @flow
-import React from "react";
-import { Text, View } from "react-native";
-import Animated from "react-native-reanimated";
+import React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import Animated from 'react-native-reanimated';
 
 import {
   AnimatedScrollView,
-  fixedContent
-} from "react-native-scrollview-animation";
-
-interface ProfileProps {
-  scrollValue: Animated.Value<number>;
-}
-
-class Profile extends React.Component<ProfileProps> {
-  render() {
-    const { scrollValue } = this.props;
-    const height = scrollValue.interpolate({
-      inputRange: [0, 120],
-      outputRange: [50, 20],
-      extrapolate: Animated.Extrapolate.CLAMP
-    });
-    const top = scrollValue.interpolate({
-      inputRange: [0, 120, 150],
-      outputRange: [180, 80, 40],
-      extrapolate: Animated.Extrapolate.CLAMP
-    });
-
-    return (
-      <Animated.Image
-        source={require("../assets/NZ.jpg")}
-        style={{
-          height,
-          width: height,
-          borderRadius: 120,
-          position: "absolute",
-          top,
-          left: 50
-        }}
-      />
-    );
-  }
-}
+  fixedContent,
+  ScalingImageComponent,
+} from 'react-native-scrollview-animation/src/index';
 
 interface Props {}
 
 class Twitter extends React.Component<Props> {
   renderHeader() {
-    return (
-      <View style={{ flex: 1, alignSelf: "stretch", backgroundColor: "red" }} />
-    );
+    return <View style={{ flex: 1, alignSelf: 'stretch', backgroundColor: 'red' }} />;
   }
   render() {
     return (
       <AnimatedScrollView
-        backgroundComponents={[]}
-        foregroundComponents={[
+        backgroundComponents={[
           fixedContent({
             render: this.renderHeader,
-            minHeight: 80,
-            maxHeight: 200
+            minHeight: 200,
+            maxHeight: 200,
           }),
-          Profile
         ]}
+        foregroundComponents={[]}
         childrenComponents={[]}
         minHeight={0}
-        maxHeight={200}
+        maxHeight={0}
         bounces={false}
       >
-        <Text style={{ height: 20000, paddingTop: 80 }}>Content</Text>
+        <View>
+          <Image
+            source={require('../assets/NZ.jpg')}
+            style={{ height: 100, alignSelf: 'stretch' }}
+          />
+          <ScalingImageComponent
+            image={require('../assets/cutecat.jpg')}
+            minHeight={50}
+            maxHeight={100}
+            style={styles.containerStyle}
+            imageStyle={styles.image}
+          />
+          <Text style={{ height: 20000 }}>Content</Text>
+        </View>
       </AnimatedScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    marginLeft: 20,
+  },
+  image: {
+    borderWidth: 2,
+    borderColor: 'white',
+  },
+});
 export default Twitter;
